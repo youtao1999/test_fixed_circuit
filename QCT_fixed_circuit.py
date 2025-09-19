32ceee162d13bfcd2220c84f55fffbfecb0ad1c3
youtao1999 <ty296@physics.rutgers.edu>
2025-09-19 11:58:42 -0400

    cleaned unnecessary files

--- QCT_fixed_circuit.py
+++ QCT_fixed_circuit.py
@@ -1,8 +1,37 @@
+import os
+import re
+import sys
+current_dir = os.getcwd()
+base_dir = re.sub(r'/test_fixed_circuit$', '', current_dir)
+# sys.path.append("/home/youtao/CT_toy")
+sys.path.append(base_dir + "/CT_toy")
 import numpy as np
 from fractions import Fraction
-from QCT_util import Haar_state, dec2bin, U, U_simp
+from QCT_util import dec2bin, U_simp
 from functools import partial
 import scipy.sparse as sp   
+import json
+
+def read_circuit():
+    with open('circuit.json', 'r') as f:
+        circuit = json.load(f)
+    return circuit
+
+def read_initial_state():
+    with open('initial_state.json', 'r') as f:
+        initial_state = np.array(json.load(f))
+    return initial_state
+
+def postprocess_bernoulli(circuit):
+    circuit_copy = []
+    for cir in circuit:
+        if cir[0] == "B":
+            matrix = np.array([[complex(cir[1][i][j]["re"], cir[1][i][j]["im"]) for j in range(4)] for i in range(4)])
+            modified_cir = ["B", matrix]
+            circuit_copy.append(modified_cir)
+        else:
+            circuit_copy.append(cir)
+    return circuit_copy
 
 class QCT_fixed_circuit():
     def __init__(self, circuit, initial_state):
@@ -198,3 +227,23 @@ class QCT_fixed_circuit():
         self.state /= np.linalg.norm(self.state)
         # print(i, applied_operator)
         return self.state, applied_operator
+
+
+if __name__ == "__main__":
+    initial_state = read_initial_state()
+    circuit = read_circuit()
+    # print(circuit[0])
+    circuit_postprocessed = postprocess_bernoulli(circuit)
+    # print(circuit_postprocessed[0])
+    # print([cir[0] for cir in circuit_postprocessed])
+    # print(circuit_postprocessed[2])
+    # print(initial_state.flatten())
+    qct = QCT_fixed_circuit(
+        circuit=circuit_postprocessed,
+        initial_state=initial_state,
+    )
+    for i in range(len(circuit_postprocessed))[0:1]:
+        state, _ = qct.step_evolution(i)
+        # print(state.flatten()[0:9])
+        # print(qct.state.flatten()[0:9])
+    # print(qct.state.flatten())
